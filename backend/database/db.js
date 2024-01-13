abc915d9a60f946c989460908550fbfaa3025553
Alec Bhaskaran <alecbhas@gmail.com>
2024-01-13 16:31:52 -0500

    Mongo bongo

---
+++ backend/database/db.js
@@ -0,0 +1,3 @@
+import mongoose from 'mongoose';
+mongoose.connect('mongodb+srv://alecb30994:<password>@pl-cluster.rypdh4y.mongodb.net/?retryWrites=true&w=majority', { 
+    useNewUrlParser: true, useCreateIndex: true });
